42a440ca6bb64f97f51f32953a723351960b22df
Aitor Cabezas <aitor.cabezas.millan@alumnos.upm.es>
2025-12-22 15:55:53 +0100

    update

--- test/test_CompressibleFlow/LaunchParallel.sh
+++ test/test_CompressibleFlow/LaunchParallel.sh
@@ -7,9 +7,9 @@
 # Editable options:
 #SBATCH --job-name=SmoothVortex
 #SBATCH --partition=cn2
-#SBATCH --array=1-24%7
+#SBATCH --array=1-24%10
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
+#SBATCH --mem=20G
 
 # Do not modify:
 #SBATCH --nodes=1
