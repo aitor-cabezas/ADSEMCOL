550ff9f66417976c741af7df3c4b58183d08dd3a
Aitor Cabezas <aitor.cabezas.millan@alumnos.upm.es>
2026-03-18 14:58:00 +0100

    update

--- test/PlotResults/SmoothVortex_PlotResults.jl
+++ test/PlotResults/SmoothVortex_PlotResults.jl
@@ -960,7 +960,8 @@ function CompareTMS_SmoothVortex(StudyCase::String;
         #TimeAdapt:NO
         
 #         SCvv1       = [1256:1259]
-        SCvv1       = [1260:1262]
+        SCvv1       = [1260:1263]
+#         SCvv1       = [1264:1267]
         
         nb          = 1
         
--- test/test_CompressibleFlow/LaunchParallel.sh
+++ test/test_CompressibleFlow/LaunchParallel.sh
@@ -7,9 +7,9 @@
 # Editable options:
 #SBATCH --job-name=SmoothVortex
 #SBATCH --partition=cn2
-#SBATCH --array=265-268%4
+#SBATCH --array=265-268%1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=60G
+#SBATCH --mem=240G
 
 # Do not modify:
 #SBATCH --nodes=1
