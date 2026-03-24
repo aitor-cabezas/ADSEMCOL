d6f65510c80487584153a55c093bf035a60bfabb
Aitor Cabezas <aitor.cabezas.millan@alumnos.upm.es>
2026-03-24 12:56:00 +0100

    update

--- test/test_ConvectionDiffusionReaction/LaunchParallel.sh
+++ test/test_ConvectionDiffusionReaction/LaunchParallel.sh
@@ -7,7 +7,7 @@
 # Editable options:
 #SBATCH --job-name=NonlinearDiffusion
 #SBATCH --partition=cn2
-#SBATCH --array=26-29,46-49%10
+#SBATCH --array=26-49%10
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=20G
 
